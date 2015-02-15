3e7fe07dfd9f59c813c34580d3a4206581deee4b
Jure Žove <jure.pase.kure@gmail.com>
2015-02-15 13:15:41 +0100

    cleaned up Car

--- test/dummy/app/models/car.rb
+++ test/dummy/app/models/car.rb
@@ -1,21 +1,3 @@
 class Car < ActiveRecord::Base
-  acts_as_shareable_object [og: [app: [:id]]]
-
-  def twitter_card
-    "image"
-  end
-
-  def twitter_app_name_iphone
-    "Bookagram"
-  end
-
-  def og_app_id
-    "Some id"
-  end
-
-  def author
-    "Jure Žove"
-  end
-
-  alias_method :article_author, :author
+  acts_as_shareable_object
 end
