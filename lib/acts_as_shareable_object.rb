3fd52028c6b77e08d1765e310f6d952cf86b1bf6
Jure Žove <jure.pase.kure@gmail.com>
2015-02-15 16:50:44 +0100

    added view helpers

--- lib/acts_as_shareable_object.rb
+++ lib/acts_as_shareable_object.rb
@@ -8,8 +8,13 @@ module ActsAsShareableObject
           ActiveRecord::Base.send(:include, ActiveRecord::Acts::ShareableObject)
         end
       end
+
+      initializer 'acts_as_shareable_object.view_helpers' do
+        ActionView::Base.send(:include, ActionView::ShareableObject::ViewHelpers)
+      end
     end
   else
     ActiveRecord::Base.send(:include, ActiveRecord::Acts::ShareableObject) if defined?(ActiveRecord)
+    ActionView::Base.send(:include, ActionView::ShareableObject::ViewHelpers) if defined?(ActionView)
   end
 end
---
+++ lib/acts_as_shareable_object/action_view/shareable_object/view_helpers.rb
@@ -0,0 +1,20 @@
+module ActionView
+  module ShareableObject
+    module ViewHelpers
+      def social_meta_tags_for(object, overrides = {})
+        raise "You need to add acts_as_shareable_object to your #{object.class} model." unless object.respond_to?(:social_meta_properties)
+        # meta_tags = []
+        # # go through each namespace: :fb, :og & custom..
+        # object.graph_properties.deep_merge(:og => overrides).each do |namespace, attributes|
+        #   attributes.each do |property, values|
+        #     # in most cases 'values' will be a single value
+        #     Array(values).each do |content|
+        #       meta_tags << tag(:meta, :property => [namespace, property].join(':'), :content => content)
+        #     end
+        #   end
+        # end
+        # meta_tags.join("\n").html_safe
+      end
+    end
+  end
+end
